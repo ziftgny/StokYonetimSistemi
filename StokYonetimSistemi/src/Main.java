c9163b915ffcd0451ab42320614e4894ffbe7802
Hilmi Gündoğdu <hilmigndogdu70@gmail.com>
2023-12-28 19:03:59 +0300

    Hilmi deniyor

--- StokYonetimSistemi/src/Main.java
+++ StokYonetimSistemi/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hilmi Deniyor");
             Program program = new Program();
             if(program.login()){
                 program.basla();
